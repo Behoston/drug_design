167fb553326e4b06cb6332f7d08fef150136503e
Behoston <mlegiecki@gmail.com>
2016-10-23 18:52:55 +0200

    Przepisanie na obiekty, dodane pobieranie z ZINC15

--- lab_2.py
+++ lab_2.py
@@ -2,13 +2,52 @@
 import os
 import subprocess as sub
 from collections import defaultdict
+from pybel import readstring
+
+import requests
 
 
-def merge_two_dicts(x, y):
-    """Given two dicts, merge them into a new dict as a shallow copy."""
-    z = x.copy()
-    z.update(y)
-    return z
+class Ligand(object):
+    def __init__(self, chembl_id, bioactivity=None):
+        self.chembl_id = chembl_id
+        self.bioactivity = bioactivity
+        self.smile = None
+        self.mol2 = None
+        self.path_to_mol2 = None
+        self.scaffold = None
+
+    def download_smile(self):
+        from chembl_webresource_client import CompoundResource
+        cr = CompoundResource()
+        self.smile = cr.get(self.chembl_id)['smiles']
+
+    def from_smile_to_mol2(self):
+        smi = readstring('smi', str(self.smile))
+        smi.make3D()
+        self.mol2 = smi
+
+    def save_mol2(self, path=None):
+        self.path_to_mol2 = path
+        self.mol2.write('mol2', self.path_to_mol2, True)
+
+    def make_scaffold(self):
+        csv_file = 'output/scaffold.csv'
+        command = 'strip-it --input ' + self.path_to_mol2 + ' --noLog --output ' + csv_file + ' 2>' + os.devnull
+        sub.call(command, shell=True)
+        self.scaffold = {}
+        with open(csv_file) as f:
+            headers = f.readline().strip().split()[2:]
+            values = f.readline().strip().split()[2:]
+            for j in xrange(len(headers)):
+                self.scaffold[headers[j]] = values[j]
+        os.remove(csv_file)
+
+
+def download_ligands_with_scaffold(smile, how_many=10):
+    url = 'http://zinc15.docking.org/substances.csv'
+    params = {'count': how_many, 'structure-contains': smile}
+    request = requests.get(url, params=params)
+    return [item.strip().split(',') for item in request.text.split('\n')[1:-1]]
 
 
 def get_ic50_for_chembl_id(chembl_id):
@@ -18,52 +57,29 @@ def get_ic50_for_chembl_id(chembl_id):
     activities = tr.bioactivities(chembl_id)
     for a in activities:
         if a['bioactivity_type'] == 'IC50':
-            result.append(a)
+            result.append(Ligand(a['ingredient_cmpd_chemblid'], a))
     return result
-
-
-def download_smile_by_chembl_id(chembl_id):
-    from chembl_webresource_client import CompoundResource
-    cr = CompoundResource()
-    ligand = cr.get(chembl_id)
-    return ligand
-
-
-def from_smile_to_mol2(smile):
-    from pybel import readstring
-    smi = readstring('smi', str(smile))
-    smi.make3D()
-    return smi
-
-
-def make_scaffold(path_to_mol3_file):
-    csv = 'output/scaffold.csv'
-    command = 'strip-it --input ' + path_to_mol3_file + ' --noLog --output ' + csv + ' 2>' + os.devnull
-    sub.call(command, shell=True)
-    scaffold = {}
-    with open(csv) as f:
-        headers = f.readline().strip().split()[2:]
-        values = f.readline().strip().split()[2:]
-        for j in xrange(len(headers)):
-            scaffold[headers[j]] = values[j]
-    os.remove(csv)
-    return scaffold
 
 
 if __name__ == '__main__':
     chembl_id = 'CHEMBL1293196'
     ligands = get_ic50_for_chembl_id(chembl_id)
     ligands_number = len(ligands)
-    for i in xrange(ligands_number):
-        l = ligands[i]
-        ligands[i] = merge_two_dicts(download_smile_by_chembl_id(l['ingredient_cmpd_chemblid']), l)
-    for i in xrange(ligands_number):
-        ligands[i]['molecule'] = from_smile_to_mol2(ligands[i]['smiles'])
-        ligands[i]['path_to_mol2_file'] = str(os.path.join('output', ligands[i]['ingredient_cmpd_chemblid'])) + '.mol2'
-        ligands[i]['molecule'].write('mol2', ligands[i]['path_to_mol2_file'], True)
     result = defaultdict(int)
-    for i in xrange(ligands_number):
-        ligands[i]['scaffold'] = make_scaffold(ligands[i]['path_to_mol2_file'])
-        result[ligands[i]['scaffold']['RINGS_WITH_LINKERS_1']] += 1
+    for l in ligands:
+        l.download_smile()
+        l.from_smile_to_mol2()
+        l.save_mol2(str(os.path.join('output', l.chembl_id + '.mol2')))
+        l.make_scaffold()
+        result[l.scaffold['RINGS_WITH_LINKERS_1']] += 1
+    best = None
+    print 'All scaffolds: '
     for k, v in result.items():
-        print v, '', '', k
+        print '\t', v, '', '', k
+        if not best or best[0] < v:
+            best = (v, k)
+    print 'Best scaffold is:', best[1]
+    how_may_ligands = 10
+    print 'First', how_may_ligands, 'ligands with best scaffolda:'
+    for row in download_ligands_with_scaffold(best[1], how_may_ligands):
+        print '\t', row
